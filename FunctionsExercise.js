f40bdb808ee9e1f85c55a1a38303de28dca1f670
Allie Baughn <allison.baughn@gmail.com>
2021-10-21 09:03:19 -0500

    Completed main exercise, not bonus

---
+++ FunctionsExercise.js
@@ -0,0 +1,66 @@
+//1
+function spookyGhost(){
+    console.log(`Boo!!!`)
+};
+spookyGhost();
+
+//2
+function Candy(goal){
+console.log(goal, goal, goal, goal);
+};
+Candy(`Get Candy`);
+
+//3
+function werewolf(a, b){
+if (`a === Full`, `b === Moon`);
+};
+werewolf(`ARHWOO!!!`);
+console.log((`a === Full`, `b === Moon`));
+
+//4
+function halloween(){
+    return `October 31st`;
+}
+halloween();
+console.log(halloween());
+
+//5
+function isEnoughCandy(pieces){
+    if (pieces >= 31){
+        return true;
+      } else {
+        return false;
+      }
+}
+
+//6
+function candyCounter(nums){
+//??????
+}
+
+//7
+const hauntedMansion = function(){
+    console.log("Welcome, foolish mortals, to the Haunted Mansion!");
+}
+hauntedMansion();
+
+//8
+ghostbusters = () => `Who You Gonna Call?`
+console.log(ghostbusters());
+
+//9
+no
+
+//10
+yes
+
+//11a
+const costumes = [
+    `Ghost`,
+    `Princess`,
+    `Pirate`
+];
+
+//11b
+const upperCostumes = costumes.map(i => i.toUpperCase());
+console.log(upperCostumes);
